b156cade5e62d605641cb60edfc3af7b3ad002ad
Azat Minimullin <minimullin.azat@yandex.ru>
2022-06-25 15:52:46 +0300

    add Tesk 004 project

---
+++ Tesk004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+//               0   1   2
+int[] array = { 44 , 5 , 78 };
+
+int result = Max(array[0], array[1], array[2]);
+Console.WriteLine(result);
+
